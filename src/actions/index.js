2a3c07e0a69d496886c15f681031d3167de22fb3
shjang7 <lucy.sh.jang@gmail.com>
2020-02-04 00:14:57 +0900

    Update not using proxy setting

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,32 +8,32 @@ import {
 } from './../assets/types';
 
 export const getBooks = () => async dispatch => {
-  return await axios.get('/api/v1/books')
+  return await axios.get(`/api/v1/books`)
     .then(({ data }) => {
       if (!data || !data.books) throw String('connection error');
-      dispatch({ type: GET_BOOKS, payload: data.books });
+      return dispatch({ type: GET_BOOKS, payload: data.books });
     })
-    .catch(error => dispatch({ type: EXCEPTION_ERROR, payload: error }));
+    .catch(error => dispatch({ type: EXCEPTION_ERROR, payload: typeof error === 'string' ? error : 'server error' }));
 };
 
 export const createBook = book => async dispatch => {
-  return await axios.post('/api/v1/books', { book })
+  return await axios.post(`/api/v1/books`, { book })
     .then(({ data }) => {
-      if (!data) throw new Error('connection error');
+      if (!data) throw String('connection error');
       if (data.status !== 'created') throw String(data.errors || 'connection error');
       dispatch({ type: CREATE_BOOK, payload: data.location });
     })
-    .catch(error => dispatch({ type: EXCEPTION_ERROR, payload: error }));
+    .catch(error => dispatch({ type: EXCEPTION_ERROR, payload: typeof error === 'string' ? error : 'server error' }));
 };
 
 export const removeBook = id => async dispatch => {
   return await axios.delete(`/api/v1/books/${id}`)
     .then(({ data }) => {
-      if (!data) throw new Error('connection error');
+      if (!data) throw String('connection error');
       if (data.status !== 'destroyed') throw String(data.errors || 'connection error');
       dispatch({ type: REMOVE_BOOK, payload: id });
     })
-    .catch(error => dispatch({ type: EXCEPTION_ERROR, payload: error }));
+    .catch(error => dispatch({ type: EXCEPTION_ERROR, payload: typeof error === 'string' ? error : 'server error' }));
 };
 
 export const changeFilter = filter => ({ type: CHANGE_FILTER, filter });
